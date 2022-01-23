c6c785e3c77d4e0b1a35b874b2043968d72cb3af
Raja Beemi <beemi.raja@gmail.com>
2022-01-23 00:04:57 +0000

    test: test updated

--- _tests_/postcode.test.ts
+++ _tests_/postcode.test.ts
@@ -4,15 +4,11 @@ beforeEach(() => {
     axios.defaults.baseURL = 'http://api.postcodes.io';
 });
 
-test('test lat long', async done => {
-    try {
-        const response = await axios.get('/postcodes/' + 'SW1A 1AA');
-        console.log(response.data)
-        expect(response.data.result.latitude).toBe(51.501);
-        expect(response.data.result.longitude).toBe(-0.14);
-        done();
-    } catch (error) {
-        expect(error).toBe(null);
-        done(error);
-    }
+test('test lat long', async () => {
+    const response = await axios.get('/postcodes/' + 'SW1A 1AA');
+    console.log(response.data)
+    expect(response.status).toBe(200);
+    expect(response.data.status).toBe(200);
+    expect(response.data.result.latitude).toBe(51.501);
+    expect(response.data.result.longitude).toBe(-0.14);
 });
