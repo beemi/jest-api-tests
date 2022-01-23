a3c2a4399ad711b225fd7db25012a46780bee62e
Raja Beemi <beemi.raja@gmail.com>
2022-01-23 11:54:31 +0000

    test: added some more tests

--- _tests_/postcode.test.ts
+++ _tests_/postcode.test.ts
@@ -6,9 +6,42 @@ beforeEach(() => {
 
 test('test lat long', async () => {
     const response = await axios.get('/postcodes/' + 'SW1A 1AA');
-    console.log(response.data)
+    console.log(JSON.stringify(response.data));
     expect(response.status).toBe(200);
     expect(response.data.status).toBe(200);
     expect(response.data.result.latitude).toBe(51.501);
     expect(response.data.result.longitude).toBe(-0.14);
 });
+
+test('test lat long with postcode lowercase', async () => {
+    const response = await axios.get('/postcodes/' + 'rm176ey');
+    console.log(JSON.stringify(response.data));
+    expect(response.status).toBe(200);
+});
+
+test('test bulk lat long validations', async () => {
+    const response = await axios.post('/postcodes/', {
+        "postcodes": ["OX49 5NU", "M32 0JG", "NE30 1DP"]
+    })
+    console.log(JSON.stringify(response.data));
+    expect(response.status).toBe(200);
+    expect(response.data.result.length).toBe(3);
+});
+
+test('test bulk reverse Geocoding', async () => {
+    const response = await axios.post('/postcodes/', {
+        "geolocations": [{
+            "longitude": 0.629834723775309,
+            "latitude": 51.7923246977375
+        }, {
+            "longitude": -2.49690382054704,
+            "latitude": 53.5351312861402,
+            "radius": 1000,
+            "limit": 5
+        }]
+    })
+
+    console.log(JSON.stringify(response.data));
+    expect(response.status).toBe(200);
+});
+
---
+++ _tests_/response-objects.ts
@@ -0,0 +1,46 @@
+export interface GetLatLongResponse {
+    status: number;
+    result: Result;
+}
+
+export interface Result {
+    postcode: string;
+    quality: number;
+    eastings: number;
+    northings: number;
+    country: string;
+    nhs_ha: string;
+    longitude: number;
+    latitude: number;
+    european_electoral_region: string;
+    primary_care_trust: string;
+    region: string;
+    lsoa: string;
+    msoa: string;
+    incode: string;
+    outcode: string;
+    parliamentary_constituency: string;
+    admin_district: string;
+    parish: string;
+    admin_county?: null;
+    admin_ward: string;
+    ced?: null;
+    ccg: string;
+    nuts: string;
+    codes: Codes;
+}
+
+export interface Codes {
+    admin_district: string;
+    admin_county: string;
+    admin_ward: string;
+    parish: string;
+    parliamentary_constituency: string;
+    ccg: string;
+    ccg_id: string;
+    ced: string;
+    nuts: string;
+    lsoa: string;
+    msoa: string;
+    lau2: string;
+}
